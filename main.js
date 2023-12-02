d5acb31306e6f16197bcd19de5341d262584c38a
ARS <magauiyanov04@gmail.com>
2023-12-02 16:53:59 +0600

    исправление  багов

--- main.js
+++ main.js
@@ -1,29 +1,58 @@
-const nav = document.querySelector('#nav'),
-    navBtn = document.querySelector('#nav-btn'),
-    navBtnImg = document.querySelector('#nav-btn-img'),
-    navItems = document.querySelectorAll('.nav-item');
 
-navBtn.addEventListener('click',(e) => {
-    e.preventDefault();
-    if (nav.classList.toggle('open')) {
-        navBtnImg.src = "./img/nav-close.svg";
-        document.body.style.overflow = 'hidden';
-    } else {
-        navBtnImg.src = "./img/nav-open.svg";
-        document.body.style.overflow = '';
-    }
-});
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+
+            const targetId = this.getAttribute('href').substring(1);
+            const targetElement = document.getElementById(targetId);
+
+            if (targetElement) {
+                targetElement.scrollIntoView({
+                    behavior: 'smooth'
+                });
+
+                // Очищаем хеш в адресной строке
+                history.replaceState({}, document.title, window.location.pathname);
+            }
+        });
+    });
+
+    document.querySelectorAll('.parent-container').forEach(container => {
+        container.addEventListener('click', function() {
+            // Выполните нужное действие при клике на родительский элемент
+            window.location.href = this.querySelector('a').href;
+        });
+    });
+
+    const nav = document.querySelector('#nav'),
+        navBtn = document.querySelector('#nav-btn'),
+        navBtnImg = document.querySelector('#nav-btn-img'),
+        navItems = document.querySelectorAll('.nav-item');
+
+    navBtn.addEventListener('click',(e) => {
+        e.preventDefault();
+        if (nav.classList.toggle('open')) {
+            navBtnImg.src = "./img/nav-close.svg";
+            document.body.style.overflow = 'hidden';
+        } else {
+            navBtnImg.src = "./img/nav-open.svg";
+            document.body.style.overflow = '';
+        }
+    });
 
 
-navItems.forEach(item => {
-    item.addEventListener('click' , () => {
-             if (nav.classList.contains('open')) {
-                 nav.classList.remove('open');
-                 document.body.style.overflow = '';
-                 navBtnImg.src = "./img/nav-open.svg";
-             }
+    navItems.forEach(item => {
+        item.addEventListener('click' , () => {
+            if (nav.classList.contains('open')) {
+                nav.classList.remove('open');
+                document.body.style.overflow = '';
+                navBtnImg.src = "./img/nav-open.svg";
+            }
+        });
     });
 });
+
 
 AOS.init({
     // Global settings:
